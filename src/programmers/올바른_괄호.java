993711482a314d75a1f5b58189b6e5f38676a748
CHOIY3SUNG <choiyesung2848@naver.com>
2022-10-23 00:08:17 +0900

    success

--- src/programmers/올바른_괄호.java
+++ src/programmers/올바른_괄호.java
@@ -3,12 +3,12 @@ import java.util.*;
 
 public class 올바른_괄호 {
     public static boolean main(String[] args) {
-        String s = "()()";
+        String s = "()()";  // Test 하기 위해..
 
         Stack<String> stack = new Stack<>();
         for(int i = 0; i<=s.length()-1; i++){
             if(s.charAt(i) == '('){
-                stack.push(String.valueOf(s.charAt(i)));
+                stack.push(String.valueOf(s.charAt(i))); // or stack.push("(");   깔끔하다!
             } else {
                 if (stack.isEmpty()){
                     return false;
